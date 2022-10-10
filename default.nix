86671478ca52e17ed10210454633465857c3f9b5
Sven Tennie <sven.tennie@gmail.com>
2022-10-10 11:25:27 +0200

    Update default.nix
    Some packages were added to the cabal file. This reflects the change on
    nix level.

--- default.nix
+++ default.nix
@@ -1,15 +1,25 @@
-{ mkDerivation, aeson, base, bytestring, http-api-data, http-client
-, http-types, jose, lens, lib, mtl, servant, servant-server, text
-, time, unordered-containers, wai
+{ mkDerivation, aeson, base, bytestring, cryptonite, hspec
+, hspec-discover, hspec-wai, http-api-data, http-client, http-types
+, jose, lens, lens-aeson, lib, mtl, servant, servant-server
+, string-conversions, text, time, transformers
+, unordered-containers, wai, wai-extra
 }:
 mkDerivation {
   pname = "servant-oauth-server";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base bytestring http-api-data http-client http-types jose
-    lens mtl servant servant-server text time unordered-containers wai
+    aeson base bytestring cryptonite http-api-data http-client
+    http-types jose lens mtl servant servant-server string-conversions
+    text time unordered-containers wai
   ];
+  testHaskellDepends = [
+    aeson base bytestring cryptonite hspec hspec-wai http-api-data
+    http-client http-types jose lens lens-aeson mtl servant
+    servant-server string-conversions text time transformers
+    unordered-containers wai wai-extra
+  ];
+  testToolDepends = [ hspec-discover ];
   homepage = "https://github.com/george-steel/servant-oauth-server#readme";
   description = "OAuth2 bearer token auth and token endpoint for Servant";
   license = lib.licenses.bsd3;
